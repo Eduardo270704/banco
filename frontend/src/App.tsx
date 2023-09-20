a64a62d666bdf6e031323d33aca303f761980612
Eduardo270704 <luis270704@gmail.com>
2023-09-19 22:00:36 -0300

    modelo de banco

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import Brand from "./pages/Brand";
+import Category from "./pages/Category";
+import User from "./pages/User";
+import Bike from "./pages/Bike";
+
+function App() {
+  return (
+    <div>
+      <Brand />
+      <Category />
+      <User />
+      <Bike />
+    </div>
+  );
+}
+
+export default App;
